6a78a81eefb1a60584cdcacbbff168aad7c7f46e
Evan Hallmark <ehallmar@usc.edu>
2025-03-21 18:33:13 -0700

    imports

--- src/lightning_client/lightning_pb2_grpc.py
+++ src/lightning_client/lightning_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import lightning_pb2 as lightning__pb2
+import lightning_client.lightning_pb2 as lightning__pb2
 
 GRPC_GENERATED_VERSION = '1.71.0'
 GRPC_VERSION = grpc.__version__
